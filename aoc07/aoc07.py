fafd6368da8654f0088f66e21a9607020ffc3957
Adam Moerschell <adam@moerschell.net>
2018-12-06 23:36:38 -0800

    Day 7 part 2

--- aoc07/aoc07.py
+++ aoc07/aoc07.py
@@ -4,6 +4,11 @@ import sys
 import re
 import datetime
 import string
+import copy
+
+def getSeconds(letter):
+    const = 60
+    return const + ord(letter) - ord("A") + 1
 
 assert len(sys.argv) == 2, sys.argv[0] + " requires 1 argument!"
 
@@ -28,8 +33,10 @@ inputFile.close()
 for s in steps:
     print s, " <- ", str(steps[s])
 
+
+# Part 1
 stepOrder = []
-remainingSteps = dict(steps)
+remainingSteps = copy.deepcopy(steps)
 
 while len(remainingSteps) != 0:
     print "--------------------"
@@ -50,10 +57,8 @@ while len(remainingSteps) != 0:
 
     # Execute Steps
     for s in remainingSteps:
-        i=0
-        for d in remainingSteps[s]:
-            if d == nextStep: del remainingSteps[s][i]
-            i+=1
+        for idx, d in enumerate(remainingSteps[s]):
+            if d == nextStep: del remainingSteps[s][idx]
 
 
     for s in remainingSteps:
@@ -63,23 +68,64 @@ while len(remainingSteps) != 0:
 print "".join(stepOrder)
 
 
+# Part 2
 second = 0
-remainingSteps = dict(steps)
-
+remainingSteps = copy.deepcopy(steps)
+inProgressSteps = []
+completeSteps = []
+workers = [0, 0, 0, 0, 0]  # value indicates seconds of work remaining
+workerStep = ["","","","",""]
 while len(remainingSteps) != 0:
-    print "--------------------"
+#for isdf in range(4):
+    print "-------------------"
+    print "START Second    = ", second
 
-    # Update worker status
-    # Remove complete steps
+    # Update Workers
+    for idx_w, w in enumerate(workers):
+        if w != 0:
+            workers[idx_w] -= 1
+
+            # Worker Finished
+            if workers[idx_w] == 0:
+                completedStep = workerStep[idx_w]
+                completeSteps.append(completedStep)
+
+                # remove in progress steps
+                for idx_p, s in enumerate(inProgressSteps):
+                    if s == completedStep: del inProgressSteps[idx_p]
+
+                # Free Dependency from remaining steps
+                for s in remainingSteps:
+                    for idx_r, r in enumerate(remainingSteps[s]):
+                        if r == completedStep: del remainingSteps[s][idx_r]
+
+                # Remove step as remaining
+                del remainingSteps[completedStep]
 
     # Find Ready Steps
+    readySteps = []
+    for s in remainingSteps:
+        if (len(remainingSteps[s]) == 0) and not s in inProgressSteps: readySteps.append(s)
+
+    readySteps.sort()
+
+    print "Ready Steps       = ", readySteps
 
     # Start new steps based on free workers
+    if len(readySteps) > 0:
+        for idx_w, w in enumerate(workers):
+            if w == 0:
+                step = readySteps.pop()
+                workers[idx_w] = getSeconds(step)
+                workerStep[idx_w] = step
+                inProgressSteps.append(step)
+                if len(readySteps) == 0: break
+
+    print "In Progress Steps = ", inProgressSteps
+    print "Workers         = ", workers
+    print "Workers Steps   = ", workerStep
+    print "Remaining Steps = ", remainingSteps
 
     # increment second
     second += 1
-
-
-
-
 
