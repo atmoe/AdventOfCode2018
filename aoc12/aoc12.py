4cecb48f7b48a398d86629d335d939cc821bf036
Adam Moerschell <adam@moerschell.net>
2018-12-11 23:56:09 -0800

    Day 12

---
+++ aoc12/aoc12.py
@@ -0,0 +1,188 @@
+#!/usr/bin/python
+
+import sys
+import re
+import datetime
+import string
+import copy
+
+assert len(sys.argv) == 2, sys.argv[0] + " requires 1 argument!"
+
+inputFile = open(sys.argv[1], "r")
+
+initialState = ""
+rules = {}
+rules["....."] = "."
+rules["....#"] = "."
+rules["...#."] = "."
+rules["...##"] = "."
+rules["..#.."] = "."
+rules["..#.#"] = "."
+rules["..##."] = "."
+rules["..###"] = "."
+rules[".#..."] = "."
+rules[".#..#"] = "."
+rules[".#.#."] = "."
+rules[".#.##"] = "."
+rules[".##.."] = "."
+rules[".##.#"] = "."
+rules[".###."] = "."
+rules[".####"] = "."
+rules["#...."] = "."
+rules["#...#"] = "."
+rules["#..#."] = "."
+rules["#..##"] = "."
+rules["#.#.."] = "."
+rules["#.#.#"] = "."
+rules["#.##."] = "."
+rules["#.###"] = "."
+rules["##..."] = "."
+rules["##..#"] = "."
+rules["##.#."] = "."
+rules["##.##"] = "."
+rules["###.."] = "."
+rules["###.#"] = "."
+rules["####."] = "."
+rules["#####"] = "."
+
+for line in inputFile.readlines():
+    init = re.match("^initial state: (.*)$", line)
+    rule = re.match("^([#\.]{5}) => ([#\.])$", line)
+
+    if init: initialState = init.group(1)
+    if rule: rules[rule.group(1)] = rule.group(2)
+
+inputFile.close()
+
+print "init = ", initialState
+print "-------------------------------------------"
+for idx,r in enumerate(rules):
+    print idx, ": ", r, "=>", rules[r]
+
+print "-------------------------------------------"
+
+initialState = "....." + initialState + "....."
+
+# Part 1
+'''
+cumulativeFilledPots = 0
+firstPotIndex = -5
+generations = []
+generations.append(initialState)
+
+print 0, ": ", generations[0]
+for g in range(1,21):
+    prevGen = generations[g-1]
+    newGen = ".."
+
+    for p in range(2,len(prevGen) - 2):
+        ruleIdx = prevGen[p-2:p+3]
+        newGen += rules[ruleIdx]
+
+    # grow in positive direction
+    if   newGen[-1] != ".": newGen += "..."
+    elif newGen[-2] != ".": newGen += ".."
+    elif newGen[-3] != ".": newGen += "."
+    newGen += ".."
+
+    # Grow in negative direction
+    if newGen[2] == "#":
+        newGen = "..." + newGen
+        firstPotIndex -= 3
+    elif newGen[3] == "#":
+        newGen = ".." + newGen
+        firstPotIndex -= 2
+    elif newGen[4] == "#":
+        newGen = "." + newGen
+        firstPotIndex -= 1
+
+    assert newGen[0:5] == ".....", "Not enough negative empty pots.  Gen = " + newGen + " gen = " + str(g)
+    assert newGen[-5:] == ".....", "Not enough positive empty pots.  Gen = " + newGen
+
+    cumulativeFilledPots += newGen.count("#")
+
+    print g, ": ", newGen
+    generations.append(newGen)
+
+print "zeroIdx = ", firstPotIndex, " -> ", generations[-1]
+
+potSum = 0
+for i in range(len(generations[-1])):
+    if generations[-1][i] == "#": potSum += i + firstPotIndex
+
+print "Pot Sum = ", potSum
+'''
+
+# Part 2
+cumulativeFilledPots = 0
+firstPotIndex = -5
+prevGen = initialState
+prevPotSum = 0
+lastGen = 0
+for i in range(len(prevGen)):
+    if prevGen[i] == "#": prevPotSum += i + firstPotIndex
+
+for g in range(1,20000):
+    if g % 10000 == 0: print "progress = ", g, " ", g/float(50000000000)
+    newGen = ".."
+
+    for p in range(2,len(prevGen) - 2):
+        ruleIdx = prevGen[p-2:p+3]
+        newGen += rules[ruleIdx]
+
+    # grow in positive direction
+    if   newGen[-1] != ".": newGen += "..."
+    elif newGen[-2] != ".": newGen += ".."
+    elif newGen[-3] != ".": newGen += "."
+    newGen += ".."
+
+    # Grow in negative direction
+    if newGen[2] == "#":
+        newGen = "..." + newGen
+        firstPotIndex -= 3
+    elif newGen[3] == "#":
+        newGen = ".." + newGen
+        firstPotIndex -= 2
+    elif newGen[4] == "#":
+        newGen = "." + newGen
+        firstPotIndex -= 1
+
+    # Trim pots
+    firstPotWithPlant = 0
+    for i in range(len(newGen)):
+        if newGen[i]=="#":
+            firstPotWithPlant = i
+            break
+
+    newGen = newGen[firstPotWithPlant-5:]
+    firstPotIndex += (firstPotWithPlant-5)
+
+    #print len(newGen)
+    #print g, " ", firstPotIndex, ": ", newGen
+
+    potSum = 0
+    for i in range(len(newGen)):
+        if newGen[i] == "#": potSum += i + firstPotIndex
+
+    print g, "firstIdx = ", firstPotIndex, " Pot Sum = ", potSum, " Prev Sum = ", prevPotSum, " diff = ", potSum - prevPotSum, " gen = ", newGen
+
+    assert newGen[0:5] == ".....", "Not enough negative empty pots.  Gen = " + newGen + " gen = " + str(g)
+    assert newGen[-5:] == ".....", "Not enough positive empty pots.  Gen = " + newGen
+ 
+    lastGen = g
+    if(prevGen == newGen): break
+
+    prevGen = newGen
+    prevPotSum = potSum
+
+# adjust first index
+print "last gen = ", lastGen, " remaining Generations = ", 50000000000 - lastGen
+firstPotIndex += 50000000000 - lastGen
+
+
+potSum = 0
+for i in range(len(prevGen)):
+    if prevGen[i] == "#": potSum += i + firstPotIndex
+
+print "Pot Sum = ", potSum
+
